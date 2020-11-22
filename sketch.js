7bef7b424ca5c8d58baa24fc4b63fa4a9bdeac47
saiabinav <72210490+saiabinav@users.noreply.github.com>
2020-11-22 14:17:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+var bullets,wall,thickness;
+var speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+
+  speed=random(223,321);
+ weight=random(30,52);
+ thickness=random(22,83);
+ 
+
+  bullets=createSprite(50,200,50,50);
+
+  wall=createSprite(1200,200,thickness,height/2);
+  wall.shapeColor=color(80,80,80,80);
+
+  bullets.velocityX=speed;
+
+ 
+
+  
+}
+
+function draw() {
+  background(255,255,255);  
+
+  if(hascollided(bullets,wall))
+
+  {
+    bullets.velocityX=0;
+    var damage =0.5*weight*speed*speed/(thickness*thickness*thickness);
+
+    if(damage>10)
+    {
+       wall.shapeColor=color(255,0,0)
+    }
+
+    
+    if(damage<10)
+    {
+     wall.shapeColor=color(0,255,0)
+    }
+
+
+  }
+  
+
+  
+  drawSprites();
+}
+
+function hascollided(bullet,wall)
+{
+    bulletRightEdge=bullet.x+bullet.width;
+    wallLeftEdge=wall.x;
+    if(bulletRightEdge>=wallLeftEdge)
+    {
+     
+      return true
+
+    }
+
+    return false;
+
+}
